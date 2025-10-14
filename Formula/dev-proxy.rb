7f9dbf3f589b33b29bb935da825af4e14fd1074a
Waldek Mastykarz <waldek@mastykarz.nl>
2025-10-14 09:08:16 +0200

    v1.2.1

--- Formula/dev-proxy.rb
+++ Formula/dev-proxy.rb
@@ -1,11 +1,11 @@
 class DevProxy < Formula
-  proxyVersion = "1.2.0"
+  proxyVersion = "1.2.1"
   if OS.linux?
     proxyArch = "linux-x64"
-    proxySha = "707ce2db113f3b224ee88f6b2ef144378bf29a683c8437262ac82f6f15459e06"
+    proxySha = "12329853c5383e71c4c09ed551b1a782396ae3a69bc071e9ceb359c26a882531"
   else
     proxyArch = "osx-x64"
-    proxySha = "e59d10d37fd0bfa2ac024bc4904fa2a44627f5fa45f78079ea915da12bf36588"
+    proxySha = "d5a4fc419457c6899b55e61dde9ee505550b9c55b1face9e50b0295353630155"
   end
 
   desc "Dev Proxy #{proxyVersion}"
